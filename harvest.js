bbc95e6f9bfbccd55a701d3fc1cde06ba5dad605
Graham Leach-Krouse <gleachkr@gmail.com>
2022-02-13 15:12:05 -0600

    Abstract out harvester class

---
+++ harvest.js
@@ -0,0 +1,98 @@
+import si from 'search-index' // https://github.com/fergiemcdowall/search-index
+import pkg from 'oai-pmh' // https://github.com/paperhive/oai-pmh
+import * as fs from 'fs/promises'
+const { OaiPmh } = pkg
+
+export default class Harvester {
+  constructor({PUT, DELETE, DOCUMENT_COUNT }) {
+    this.PUT = PUT
+    this.DELETE = DELETE
+    this.DOCUMENT_COUNT = DOCUMENT_COUNT
+  }
+
+  harvest = async _ => {
+    let lastRun 
+    const msecs = await fs.readFile('lastRun.txt')
+    try {
+      const msecs = await fs.readFile('lastRun.txt')
+      lastRun = new Date(parseInt(msecs.toString(), 10))
+    } catch {
+      lastRun = new Date("1990-01-01")
+    }
+    const dateNow = Date.now()
+    const oaiPmh = new OaiPmh('http://philarchive.org/oai.pl')
+    let recordIterator
+    recordIterator = oaiPmh.listRecords({
+      metadataPrefix: 'oai_dc',
+      from: lastRun.toISOString()
+    })
+    const options = { storeVectors: true } 
+    let count = 0 
+    let deletions = 0 
+    let total = 0
+    const chunk = []
+    for await (const record of recordIterator) {
+      if (record.header?.['$']?.status === 'deleted') {
+        deletions++
+        await this.DELETE(record.header.identifier)
+      } else {
+        const data = {}
+        // We only capture dublin core right now
+        const md = record.metadata["oai_dc:dc"]
+        // need to strip dc: prefix from fields for search indexing
+        data.contributor = md?.["dc:contributor"] 
+        data.coverage = md?.["dc:coverage"] 
+        data.creator = md?.["dc:creator"] 
+        const maybeDate = Date.parse(md?.["dc:date"])
+        if (maybeDate) data.date = maybeDate //should convert to unix time for search by range
+        data.description = md?.["dc:description"] 
+        data.format = md?.["dc:format"] 
+        data.identifier = md?.["dc:identifier"] 
+        data._id = md?.["dc:identifier"] 
+        data.language = md?.["dc:language"] 
+        data.publisher = md?.["dc:publisher"] 
+        data.relation = md?.["dc:relation"] 
+        data.rights = md?.["dc:rights"] 
+        data.source = md?.["dc:source"] 
+        data.subject = md?.["dc:subject"] 
+        data.title = md?.["dc:title"] 
+        data.type = md?.["dc:type"] 
+        chunk.push(data)
+        count++
+        total = await this.DOCUMENT_COUNT()
+        console.log(`count: ${count}, total ${total}, deletions ${deletions}`)
+      }
+      if (chunk.length > 25) {
+        const results = await this.PUT(chunk, options)
+        results.map(rslt => {
+          if (rslt.status !== "CREATED") console.log(rslt)
+        })
+        chunk.length = 0
+      }
+    }
+    await this.PUT(chunk, options)
+    console.log(`count: ${count}, total ${total}, deletions ${deletions}`)
+    await fs.writeFile('lastRun.txt',dateNow.toString())
+  }
+
+  safeHarvest = _ => {
+    if (this.harvesting) return
+    this.harvesting = true
+    this.harvest()
+    .catch(e => {
+      this.harvesting = false
+      switch (e.name) {
+        case "TypeError" : {
+          console.log("hitting upstream bug for 1-entry reponses")
+          return
+        }
+        case "OaiPmhError" : {
+          console.log("no records available")
+          return
+        }
+        default : throw e
+      }
+    })
+    .then(_ => this.harvesting = false)
+  }
+}
--- index.js
+++ index.js
@@ -1,98 +1,15 @@
 import si from 'search-index' // https://github.com/fergiemcdowall/search-index
 // *very* weird bug, importing oai-pmh before search-index causes a segfault
-import pkg from 'oai-pmh' // https://github.com/paperhive/oai-pmh
-import * as fs from 'fs/promises'
+import Harvester from './harvest.js'
 import http from 'http'
-
-const { OaiPmh } = pkg
-
+ 
 console.log("initializing...")
 
-const { PUT, DELETE, QUERY, DOCUMENT_COUNT } = await si()
+const { PUT, QUERY, DELETE, DOCUMENT_COUNT } = await si()
 
-var lastRun 
+const harvester = new Harvester({ PUT, DELETE, DOCUMENT_COUNT })
 
-try {
-  const msecs = await fs.readFile('lastRun.txt')
-  lastRun = new Date(parseInt(msecs.toString(), 10))
-} catch {
-  lastRun = new Date("1990-01-01")
-}
-
-async function retrieve() {
-   const dateNow = Date.now()
-   const oaiPmh = new OaiPmh('http://philarchive.org/oai.pl')
-   let recordIterator
-   recordIterator = oaiPmh.listRecords({
-     metadataPrefix: 'oai_dc',
-     from: lastRun.toISOString()
-   })
-   const options = { storeVectors: true } 
-   let count = 0 
-   let deletions = 0 
-   let total = 0
-   const chunk = []
-   for await (const record of recordIterator) {
-     if (record.header?.['$']?.status === 'deleted') {
-       deletions++
-       await DELETE(record.header.identifier)
-     } else {
-       const data = {}
-       // We only capture dublin core right now
-       const md = record.metadata["oai_dc:dc"]
-       // need to strip dc: prefix from fields for search indexing
-       data.contributor = md?.["dc:contributor"] 
-       data.coverage = md?.["dc:coverage"] 
-       data.creator = md?.["dc:creator"] 
-       const maybeDate = Date.parse(md?.["dc:date"])
-       if (maybeDate) data.date = maybeDate //should convert to unix time for search by range
-       data.description = md?.["dc:description"] 
-       data.format = md?.["dc:format"] 
-       data.identifier = md?.["dc:identifier"] 
-       data._id = md?.["dc:identifier"] 
-       data.language = md?.["dc:language"] 
-       data.publisher = md?.["dc:publisher"] 
-       data.relation = md?.["dc:relation"] 
-       data.rights = md?.["dc:rights"] 
-       data.source = md?.["dc:source"] 
-       data.subject = md?.["dc:subject"] 
-       data.title = md?.["dc:title"] 
-       data.type = md?.["dc:type"] 
-       chunk.push(data)
-       count++
-       total = await DOCUMENT_COUNT()
-       console.log(`count: ${count}, total ${total}, deletions ${deletions}`)
-     }
-     if (chunk.length > 25) {
-       const results = await PUT(chunk, options)
-       results.map(rslt => {
-         if (rslt.status !== "CREATED") console.log(rslt)
-       })
-       chunk.length = 0
-     }
-   }
-   await PUT(chunk, options)
-   console.log(`count: ${count}, total ${total}, deletions ${deletions}`)
-   await fs.writeFile('lastRun.txt',dateNow.toString())
-}
-
-function safeRetrieve() {
-  retrieve().catch(e => {
-    switch (e.name) {
-      case "TypeError" : {
-        console.log("hitting upstream bug for 1-entry reponses")
-        return
-      }
-      case "OaiPmhError" : {
-        console.log("no records available")
-        return
-      }
-      default : throw e
-    }
-  })
-}
-
-setInterval(safeRetrieve, 10000)
+setInterval(harvester.safeHarvest, 10000)
 
 const server = http.createServer((req,res) => {
   switch (req.method) {
